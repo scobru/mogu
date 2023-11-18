bb8b6a7c3b2fed5bd6ce306f4f167e8547d0c690
scobru <scobru1988@gmail.com>
2023-11-18 22:33:05 +0100

    Delete unnecessary files

--- dist/tests/testsdk.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sdk_1 = require("../sdk/sdk");
-const mogu = new sdk_1.Mogu("", process.env.PINATA_API_KEY, process.env.PINATA_API_SECRET);
-async function test() {
-    const state = await mogu.load("");
-    console.log(state);
-}
-test();
